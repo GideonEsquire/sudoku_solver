86deb6efc92faf8f488a7fc5e3661d9defa2e101
GideonEsquire <GideonEsquire@gmail.com>
2016-10-31 10:47:54 -0500

    Able to display numbers.

--- cells.js
+++ cells.js
@@ -1,4 +1,7 @@
-function Cell() {
+function Cell(x, y, val) {
+    this.x = x;
+    this.y = y;
+    this.final_value = val;
     this.possible_values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,26 @@
 var cells = [];
+var solved = false;
 
 function setup() {
+
+    // Scaling depending on screen width or height
     createCanvas(windowWidth, windowHeight);
-    var unit = floor(windowHeight / 10 - 10);
+    var unit = floor(windowHeight / 11);
+    if (windowWidth < windowHeight) {
+        unit = floor(windowWidth / 11);
+    }
+
+    rectMode(CENTER);
+    textSize(unit / 2);
+    textAlign(CENTER);
     
+    // Draw grid
     strokeWeight(2);
     for (var i = 0; i < 9; i++) {
         cells[i] = [];
         for (var j = 0; j < 9; j++) {
-            cells[i].push(new Cell());
-            rect(unit*i + unit, unit*j + unit,unit,unit);
+            cells[i].push(new Cell(unit*i+unit*1.5, unit*j+unit*1.5));
+            rect(unit*i+unit*1.5, unit*j+unit*1.5,unit,unit);
         }
     }
 
@@ -17,26 +28,25 @@ function setup() {
     strokeWeight(4);
     for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
-            rect(unit*i*3 + unit, unit*j*3 + unit,unit*3,unit*3);
+            rect(unit*i*3 + unit*2.5, unit*j*3 + unit*2.5,unit*3,unit*3);
         }
     }
+    fill(0);
 
 
 }
 
 function draw() {
 
+    // logic for printing final values. Move out of here
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (cells[i][j].final_value) {
+                text(cells[i][j].final_value, cells[i][j].x, cells[i][j].y);
+            }
+        
+        }
+    }
 
-}
-
-function check_row() {
-
-}
-
-function check_col() {
-
-}
-
-function check_block() {
 
 }
