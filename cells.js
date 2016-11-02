b1fff0d3774098d2cc0dca921a1916074a8163dc
GideonEsquire <GideonEsquire@gmail.com>
2016-11-02 10:57:34 -0500

    A little logic added

--- cells.js
+++ cells.js
@@ -4,5 +4,11 @@ function Cell(x, y, val) {
     this.final_value = val;
     this.possible_values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+
+    this.produce_number = function() {
+        this.final_value = this.possible_values[0];
+        text(this.final_value,this.x,this.y);
+    }
+
 }
 
--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var solved = false;
 
 function setup() {
 
+    frameRate(5);
+
     // Scaling depending on screen width or height
     createCanvas(windowWidth, windowHeight);
     var unit = floor(windowHeight / 11);
@@ -15,6 +17,7 @@ function setup() {
     textAlign(CENTER);
     
     // Draw grid
+    push();
     strokeWeight(2);
     for (var i = 0; i < 9; i++) {
         cells[i] = [];
@@ -24,6 +27,7 @@ function setup() {
         }
     }
 
+    push();
     noFill();
     strokeWeight(4);
     for (var i = 0; i < 3; i++) {
@@ -31,20 +35,54 @@ function setup() {
             rect(unit*i*3 + unit*2.5, unit*j*3 + unit*2.5,unit*3,unit*3);
         }
     }
-    fill(0);
+    pop();
+    pop();
 
 
 }
 
+
 function draw() {
 
-    // logic for printing final values. Move out of here
+    // Columns
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 9; j++) {
             if (cells[i][j].final_value) {
-                text(cells[i][j].final_value, cells[i][j].x, cells[i][j].y);
+                for (var jj = 0; jj < 9; jj++) {
+                    var index = cells[i][jj].possible_values.indexOf(cells[i][j].final_value);
+                    if (!cells[i][jj].final_value && index > -1) {
+                        cells[i][jj].possible_values.splice(index, 1);
+                    }
+                }
             }
-        
+        }
+    }
+
+    // Rows
+    // Need to work on all the logic
+    // NOT WORKING JUST YET
+    for (var j = 0; j < 9; j++) {
+        for (var i = 0; i < 9; i++) {
+            if (cells[i][j].final_value) {
+                for (var jj = 0; jj < 9; jj++) {
+                    var index = cells[j][jj].possible_values.indexOf(cells[i][j].final_value);
+                    if (!cells[j][jj].final_value && index > -1) {
+                        cells[j][jj].possible_values.splice(index, 1);
+                     }
+                }
+            }
+        }
+    }
+
+    // logic for printing final values.
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (cells[i][j].possible_values.length == 1) {
+                cells[i][j].produce_number();
+            }
+            else if (cells[i][j].final_value) {
+                text(cells[i][j].final_value,cells[i][j].x,cells[i][j].y);
+            }
         }
     }
 
